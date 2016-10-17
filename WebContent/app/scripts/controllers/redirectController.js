29e7570313f6a1da7aa69c2947cac2e42703d65a
Marco A. Moreira <marco.anjos@iteris.com.br>
2016-10-17 14:03:03 -0200

    adicinado nova view e retirado a antiga url dados

--- WebContent/app/scripts/controllers/redirectController.js
+++ WebContent/app/scripts/controllers/redirectController.js
@@ -3,8 +3,7 @@ angular.module('Conciliador.redirectController',[])
 .config(['$routeProvider','RestangularProvider' ,function ($routeProvider, RestangularProvider) {
 	$routeProvider
 		.when('/redirect', {
-
-			template: '<p>Carregando..</p>',
+			templateUrl: "app/views/redirect.html",
 			controller  : "redirectController"
 		});
 }])
@@ -14,13 +13,13 @@ angular.module('Conciliador.redirectController',[])
 
 	$rootScope.bodyId = "redirectPage";
 
-	//http://127.0.0.1:8100/#/redirect?dados=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhdXRoZW50aWNhdGlvbi1zZXJ2ZXIiLCJ1c3VhcmlvIjp7IkVtYWlsIjoiZ3VzdGF2by5zaWx2YUBpdGVyaXMuY29tLmJyIiwiU2VuaGEiOm51bGwsIk5vbWUiOiJHdXN0YXZvIFNvYXJlcyBkYSBTaWx2YSAtIEdTViJ9fQ.QeVSX4bjP7BLd8OFBpm9Wjfthvq_pZN3YE9Il2zSs_OButV6HyvVvpdYv07VzfXKIgB7vade3kch3RgUxdqz6owbsvXJDCQsEf12Z-ulb3l6aN1hfTj99_vI_nUvYFvMo67pKcut6st8TfhSenE0UqyfXjsQTXTFc13RFwK9LwAEyIKKZ4Y7_RnF69UaxOg3UgNS7PR2qExQxYIdTQGXLACh2Y2xmSJqadzMNHUpmKShQHAEVKLbyywunbI9J1yYP95SxrNMPwju5zamPzgxk0CPqIqi_GlGMTMR_XDimuuGdssGX9IXw3kn7C4N77LskWAjOs0RZ9xsurhfaB6ry
+	//http://127.0.0.1:8100/#/redirect?data=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhdXRoZW50aWNhdGlvbi1zZXJ2ZXIiLCJ1c3VhcmlvIjp7IkVtYWlsIjoiZ3VzdGF2by5zaWx2YUBpdGVyaXMuY29tLmJyIiwiU2VuaGEiOm51bGwsIk5vbWUiOiJHdXN0YXZvIFNvYXJlcyBkYSBTaWx2YSAtIEdTViJ9fQ.QeVSX4bjP7BLd8OFBpm9Wjfthvq_pZN3YE9Il2zSs_OButV6HyvVvpdYv07VzfXKIgB7vade3kch3RgUxdqz6owbsvXJDCQsEf12Z-ulb3l6aN1hfTj99_vI_nUvYFvMo67pKcut6st8TfhSenE0UqyfXjsQTXTFc13RFwK9LwAEyIKKZ4Y7_RnF69UaxOg3UgNS7PR2qExQxYIdTQGXLACh2Y2xmSJqadzMNHUpmKShQHAEVKLbyywunbI9J1yYP95SxrNMPwju5zamPzgxk0CPqIqi_GlGMTMR_XDimuuGdssGX9IXw3kn7C4N77LskWAjOs0RZ9xsurhfaB6ry
 
 	init();
 
 	function init() {
 
-		var authorization = getParameterByName("dados");
+		var authorization = getParameterByName("data");
 
 		if(authorization) {
 
