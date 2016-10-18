bc4fbca6d2371602c8bfdb054c3411ca53a07683
Karlisson Bezerra <kbezerra@thoughtworks.com>
2016-10-18 17:21:31 -0300

    Write test for expecting service method to be called

--- WebContent/app/tests/controllers/RelatorioVendasControllerSpec.js
+++ WebContent/app/tests/controllers/RelatorioVendasControllerSpec.js
@@ -3,7 +3,7 @@ describe('Conciliador', function() {
     beforeEach(module('KaplenWeb'));
 
     describe('RelatorioVendasController', function() {
-        var scope, httpBackend, http, controller, service;
+        var scope, controller, service;
 
         beforeEach(module(function($provide) {
             service = {
@@ -15,21 +15,14 @@ describe('Conciliador', function() {
             $provide.value('TransactionService', service);
         }));
 
-        beforeEach(inject(function ($rootScope, $controller, $httpBackend, $http) {
+        beforeEach(inject(function ($rootScope, $controller) {
             scope = $rootScope.$new();
-            httpBackend = $httpBackend;
             controller = $controller;
-            http = $http;
-
-            httpBackend.when("GET", /\/transactions\/export/).respond({
-                'status': 200,
-                'data': 'http://excel.file'
-            });
         }));
 
         it('receives an Excel file link', function() {
-            spyOn(service, 'exportTransactions').and.returnValue({data: 'http://excel.file'});
-            controller('relatorioVendasController', {$scope: scope, $http: http, TransactionService: service});
+            spyOn(service, 'exportTransactions');
+            controller('relatorioVendasController', {$scope: scope, TransactionService: service});
             scope.exportAnalytical();
             expect(service.exportTransactions).toHaveBeenCalled();
         });
