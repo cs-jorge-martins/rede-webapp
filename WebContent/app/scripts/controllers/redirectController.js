0fd91d2c78bedb3e241e7b210563569faef18d84
Marco Aurélio <marcoaure47@gmail.com>
2016-10-19 11:06:25 -0200

    arrumando nome das classes

--- WebContent/app/scripts/controllers/redirectController.js
+++ WebContent/app/scripts/controllers/redirectController.js
@@ -13,8 +13,6 @@ angular.module('Conciliador.redirectController',[])
 
 	$rootScope.bodyId = "redirectPage";
 
-	//http://127.0.0.1:8100/#/redirect?data=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhdXRoZW50aWNhdGlvbi1zZXJ2ZXIiLCJ1c3VhcmlvIjp7IkVtYWlsIjoiZ3VzdGF2by5zaWx2YUBpdGVyaXMuY29tLmJyIiwiU2VuaGEiOm51bGwsIk5vbWUiOiJHdXN0YXZvIFNvYXJlcyBkYSBTaWx2YSAtIEdTViJ9fQ.QeVSX4bjP7BLd8OFBpm9Wjfthvq_pZN3YE9Il2zSs_OButV6HyvVvpdYv07VzfXKIgB7vade3kch3RgUxdqz6owbsvXJDCQsEf12Z-ulb3l6aN1hfTj99_vI_nUvYFvMo67pKcut6st8TfhSenE0UqyfXjsQTXTFc13RFwK9LwAEyIKKZ4Y7_RnF69UaxOg3UgNS7PR2qExQxYIdTQGXLACh2Y2xmSJqadzMNHUpmKShQHAEVKLbyywunbI9J1yYP95SxrNMPwju5zamPzgxk0CPqIqi_GlGMTMR_XDimuuGdssGX9IXw3kn7C4N77LskWAjOs0RZ9xsurhfaB6ry
-
 	init();
 
 	function init() {
@@ -22,12 +20,12 @@ angular.module('Conciliador.redirectController',[])
 		var authorization = getParameterByName("data");
 
 		var errorMessage = function () {
-			var validandoAcessoDiv = document.querySelector("#validandoAcesso");
-			var erroDiv = document.querySelector("#erro");
+			var accessValidatingDiv = document.querySelector("#accessValidating");
+			var errorDiv = document.querySelector("#error");
 			var urlRedirectError = "http://userede.com.br";
 
-			validandoAcessoDiv.classList.add("hidden");
-			erroDiv.classList.remove("hidden");
+			accessValidatingDiv.classList.add("hidden");
+			errorDiv.classList.remove("hidden");
 
 			setTimeout(function () {
 				window.location.href = urlRedirectError;
