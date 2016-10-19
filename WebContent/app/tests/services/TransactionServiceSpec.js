69875a08075df8738874dfba661f03fc789f6ee9
Karlisson Bezerra <kbezerra@thoughtworks.com>
2016-10-19 14:00:48 -0300

    Criação de teste de serviço

--- WebContent/app/tests/services/TransactionServiceSpec.js
+++ WebContent/app/tests/services/TransactionServiceSpec.js
@@ -3,6 +3,23 @@ describe('Conciliador', function() {
     beforeEach(module('KaplenWeb'));
 
     describe('TransactionService', function() {
-        
+        var scope, service, httpBackend;
+
+        beforeEach(inject(function ($rootScope, $httpBackend) {
+            scope = $rootScope.$new();
+            httpBackend = $httpBackend;
+            
+        }));
+
+        it('calls the service', function(){
+            httpBackend
+            .expect('GET', /\transactions\/export/)
+            .respond(200, { data: 'foobar' });
+
+            service.exportTransactions(jasmine.any(Object), jasmine.any(Function));
+            httpBackend.flush();
+
+            expect().toBe
+        });
     });
 });
