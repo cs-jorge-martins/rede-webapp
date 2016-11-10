fc0ab9ab0bc5a47cf0be5fc6c9bc771cf8a82e62
Marco Aurélio <marcoaure47@gmail.com>
2016-11-10 10:10:12 -0200

    receipts_expected regenerado

--- WebContent/app/scripts/controllers/receiptsExpectedDetailsController.js
+++ WebContent/app/scripts/controllers/receiptsExpectedDetailsController.js
@@ -28,7 +28,6 @@ angular.module('Conciliador.receiptsExpectedDetailsController',['ui.bootstrap'])
 				$scope.shopIds = $rootScope.receiptsDetails.shopIds;
 				$scope.shops = $rootScope.receiptsDetails.shops;
 				$scope.products = $rootScope.receiptsDetails.products;
-				//$scope.type = $rootScope.receiptsDetails.type;
 				$scope.bankAccount = $rootScope.receiptsDetails.bankAccount;
 
 				$scope.expectedAmount = $rootScope.receiptsDetails.expectedAmount;
@@ -50,34 +49,18 @@ angular.module('Conciliador.receiptsExpectedDetailsController',['ui.bootstrap'])
 				$scope.tabs = [
 					{
 						title: 'Rede',
+						id: 1,
 						active: true
 					},
 					{
-						title: 'Cielo'
+						title: 'Cielo',
+						id: 2
 					},
 					{
-						title: 'GetNet'
+						title: 'GetNet',
+						id: 3
 					}
-					/*,
-					{
-						title: 'ecommerce'
-					}
-					*/
 				];
-
-				// filter = {
-				// 	cardProductIds: $scope.cardProduct.cardProductId,
-				// 	acquirerIds: $scope.acquirer.id,
-				// 	startDate: calendarFactory.formatDateTimeForService($scope.startDate),
-				// 	endDate: calendarFactory.formatDateTimeForService($scope.endDate),
-				// 	bankAccountIds: $scope.bankAccount.id
-				// };
-                //
-				// if($scope.status === 'forethought'){
-				// 	filter.status = 'FORETHOUGHT'
-				// } else {
-				// 	filter.status = 'RECEIVED'
-				// }
 
 				$scope.maxSize = 4;
 
@@ -86,53 +69,23 @@ angular.module('Conciliador.receiptsExpectedDetailsController',['ui.bootstrap'])
 				$scope.salesTotalItens = 0;
 				$scope.salesCurrentPage = 0;
 
-        		$scope.adjustsData = [];
-        		$scope.adjustsTotalItensPage = 10;
-        		$scope.adjustsTotalItens = 0;
-				$scope.adjustsCurrentPage = 0;
-
-        		$scope.cancellationsData = [];
-        		$scope.cancellationsTotalItensPage = 10;
-        		$scope.cancellationsTotalItens = 0;
-				$scope.cancellationsCurrentPage = 0;
-
-        		$scope.ecommerceData = [];
-        		$scope.ecommerceTotalItensPage = 10;
-        		$scope.ecommerceTotalItens = 0;
-				$scope.ecommerceCurrentPage = 0;
-
 				$scope.back = back;
-				$scope.getShopsLabel = getShopsLabel;
 				$scope.changeTab = changeTab;
 			}
-		}
-
-		function getShopsLabel() {
-
-			var shops = "";
-
-			if($scope.shops.length > 1) {
-				shops = $scope.shops[0].label + ' +' + ($scope.shops.length - 1) + ' estabelecimento'
-
-				if($scope.shops.length > 2) {
-					shops += 's'
-				}
-			}
-
-			return shops;
-
 		}
 
 	    function back(){
 	        $location.path('/receipts');
 	    }
 
-	    function getExpectedDetails(cache) {
+	    function getExpectedDetails(acquirer_id) {
 
 			filter.page =  $scope.salesCurrentPage ==  0 ? $scope.salesCurrentPage : $scope.salesCurrentPage - 1;
 			filter.size =  $scope.salesTotalItensPage;
 			filter.sort = $scope.sort;
-			filter.acquirer = "1"
+			filter.acquirer = acquirer_id;
+
+			// https://z20ycs2v3e.execute-api.us-east-1.amazonaws.com/dev/financials/details?acquirerIds=1&bankAccountIds=5&cardProductIds=1&endDate=20161004&page=0&size=10&sort=transaction.date,DESC&sort=transaction.hour,DESC&startDate=20161004&status=RECEIVED&type=CREDIT
 
 			FinancialService.getExpectedDetails(filter).then(function(response) {
 				var data = response.data.content;
@@ -147,90 +100,28 @@ angular.module('Conciliador.receiptsExpectedDetailsController',['ui.bootstrap'])
 			});
 	    }
 
-	    function getAdjusts(cache, order) {
-			filter.type = 'ADJUST';
-			filter.page =  $scope.adjustsCurrentPage ==  0 ? $scope.adjustsCurrentPage : $scope.adjustsCurrentPage - 1;
-			filter.size =  $scope.adjustsTotalItensPage;
-			filter.sort = $scope.sort;
+	    function changeTab(index, acquirer_id) {
 
-			if(order) {
-				filter.sort = order;
-			}
-
-			FinancialService.getReceipt(filter).then(function(response) {
-				var data = response.data.content;
-				var pagination = response.data.page;
-
-				$scope.adjustsData = data;
-				$scope.adjustsTotalItens = pagination.totalElements;
-			}).catch(function(response) {
-				$scope.adjustsData = [];
-				console.log('[receiptsDetailsController:getAdjusts] error');
-			});
-	    }
-
-	    function getCancellations(cache, order) {
-	    	filter.type = 'CANCELLATION';
-	    	filter.page =  $scope.cancellationsCurrentPage ==  0 ? $scope.cancellationsCurrentPage : $scope.cancellationsCurrentPage - 1;
-			filter.size =  $scope.cancellationsTotalItensPage;
-			filter.sort = $scope.sort;
-
-			if (order) {
-				filter.sort =  order;
-			}
-
-			FinancialService.getReceipt(filter).then(function(response) {
-				var data = response.data.content;
-				var pagination = response.data.page;
-
-				$scope.cancellationsData = data;
-				$scope.cancellationsTotalItens = pagination.totalElements;
-			}).catch(function(response) {
-				$scope.cancellationsData = [];
-				console.log('[receiptsDetailsController:getAdjusts] error');
-			});
-	    }
-
-	    function getEcommerce() {
-	    
-	    }
-
-	    function changeTab(index) {
 	    	$scope.tabs[index].active = true;
 			$scope.sort = "";
+			console.log(acquirer_id)
 
-	    	if(index === 0) {
-	    		getSales();
-	    	} else if(index === 1) {
-	    		getAdjusts();
-	    	} else if(index === 2) {
-	    		getCancellations();
-	    	} else if(index === 0) {
-	    		getCommerce();
-	    	}
+			getExpectedDetails(acquirer_id);
+
 	    }
 
-	    $scope.sortResults = function(elem, kind, tipo_relatorio) {
-			$scope.sort = $rootScope.sortResults(elem, kind);
-			if(tipo_relatorio == "sales") {
-				getSales(false);
-			} else if(tipo_relatorio == "adjusts") {
-				getAdjusts();
-			} else if(tipo_relatorio == "cancellation") {
-				getCancellations();
-			}
+	    $scope.sortResults = function(elem, kind) {
+
 	    }
 
 	    /* pagination */
 		$scope.pageChangedSales = function () {
 			$scope.salesCurrentPage = this.salesCurrentPage;
-			getSales();
 		};
 
 		$scope.totalItensPageChangedSales = function () {
 			this.salesCurrentPage = $scope.salesCurrentPage = 0;
 			$scope.salesTotalItensPage = this.salesTotalItensPage;
-			getSales();
 		};
 
 		$scope.pageChangedAdjusts = function () {
@@ -254,4 +145,5 @@ angular.module('Conciliador.receiptsExpectedDetailsController',['ui.bootstrap'])
 			$scope.cancellationsTotalItensPage = this.cancellationsTotalItensPage;
 			getCancellations();
 		};
+
 	});
