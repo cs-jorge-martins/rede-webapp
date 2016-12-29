0892ad378116172db06e140efa027416613d31e1
gustavo.silva <gustavo.silva@iteris.com.br>
2016-12-29 17:57:59 -0200

    Header com data e chips em vendas a conciliar
    Implementado header da aba de vendas a conciliar e os chips com
    comportamento de filtro

--- WebContent/app/controllers/sales-to-reconcile-controller.js
+++ WebContent/app/controllers/sales-to-reconcile-controller.js
@@ -18,11 +18,15 @@
 
         var objVm = this;
 
+        $scope.getReceipt = GetReceipt;
+        $scope.resetFilter = ResetFilter;
+
         Init();
 
         function Init() {
             DefaultOptions();
             InitFilterVariables();
+            UpdateDateModel();
             GetFilters();
         }
         
@@ -35,7 +39,7 @@
             $scope.cardProductsData = [];
             $scope.cardProductsModel = {};
             $scope.terminalsData = [];
-            $scope.terminalsModel = {}
+            $scope.terminalsModel = {};
             $scope.pvsData = [];
             $scope.pvsModel = {};
             $scope.acquirersData = [];
@@ -59,6 +63,66 @@
                 $scope.acquirersData = filterService.TransformDeferredDataInArray(objAcquirers, 'name');
                 $scope.acquirersModel = angular.copy($scope.acquirersData);
             });
+        }
+
+        function UpdateDateModel() {
+            $scope.dateModel = {
+                day: calendarFactory.getDayOfDate($scope.date),
+                monthName: calendarFactory.getMonthNameOfDate($scope.date)
+            }
+        }
+
+        function GetLabels() {
+            $scope.terminalLabel = BuildLabel('terminal', $scope.filteredTerminals, 'is', 1);
+            $scope.terminalFullLabel = BuildTooltip($scope.filteredTerminals);
+            $scope.pvLabel = BuildLabel('estabelecimento', $scope.filteredPvs, 's', 0);
+            $scope.pvFullLabel = BuildTooltip($scope.filteredPvs);
+            $scope.acquirerLabel = BuildLabel('adquirente', $scope.filteredAcquirers, 's', 0);
+            $scope.acquirerFullLabel = BuildTooltip($scope.filteredAcquirers);
+            $scope.cardProductLabel = BuildLabel('bandeira', $scope.filteredCardProducts, 's', 0);
+            $scope.cardProductFullLabel = BuildTooltip($scope.filteredCardProducts);
+        }
+
+        function BuildLabel(strName, xModel, strSuffix, intRemoveLast) {
+            var intLength = 0;
+            var objEntity = xModel;
+            if (xModel.length) {
+                intLength = xModel.length - 1;
+                objEntity = xModel[0];
+            }
+
+            var strLabel =  strName + ': ' + objEntity.label;
+
+            if (intLength > 0) {
+                var strPluralized = strName;
+                if (intLength > 1) {
+                    strPluralized = strName.substring(0, strName.length - intRemoveLast) + strSuffix;
+                }
+
+                strLabel += ' +' + intLength + ' ' + strPluralized;
+
+                return strLabel;
+            }
+        }
+
+        function GetReceipt() {
+            $scope.filteredTerminals = angular.copy($scope.terminalsModel);
+            $scope.filteredAcquirers = angular.copy($scope.acquirersModel);
+            $scope.filteredPvs = angular.copy($scope.pvsModel);
+            $scope.filteredCardProducts = angular.copy($scope.cardProductsModel);
+
+            GetLabels();
+        }
+
+        function ResetFilter(strModel) {
+            $scope[strModel+ 'Model'] = angular.copy($scope[strModel + 'Data']);
+            GetReceipt();
+        }
+
+        function BuildTooltip(arrModel) {
+            return arrModel.map(function(objItem){
+                return objItem.label;
+            }).join(", ");
         }
 
     }
