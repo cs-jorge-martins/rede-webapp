88c47c7a6b85160a2c757e14c1d8b9f71521ee8a
gustavo.silva <gustavo.silva@iteris.com.br>
2017-01-04 17:03:08 -0200

    [dQBvVle4] DIretiva para montar checkboxes com contador

--- WebContent/app/directives/rc-checkbox/rc-checkbox.js
+++ WebContent/app/directives/rc-checkbox/rc-checkbox.js
@@ -6,26 +6,53 @@
 
 /**
  * @class Conciliador.checkbox
- * Diretiva de checkbox
+ * Diretiva de checkbox com contador
  *
- * Utiliza a diretiva checkbox para fazer marcar componentes únicos, marcar todos os componentes
- * e desmarcar todos os componentes.
+ * Utilize a diretiva rc-checkbox para marcar uma única linha de dados.
  * @param {Object} completeModel Espera o model de objeto com todas as opções disponíveis
- * @param {Object} model Model que será utilizado para compartilhar as opções checkadas com outros checkboxes
+ * @param {Object} checkModel Model que será utilizado para compartilhar as opções checkadas com outros checkboxes
  * @param {Array} responseModel que será utilizado para devolver a resposta em array
- * @param {Boolean} checkAll que será utilizado verificar se o checkbox é do tipo CheckAll, que marca/desmarca
- * todos os checkboxes do mesmo model
+ * @param {Object} quantityModel armazena a quantidade de itens marcados
+ * @param {String} quantityField usado para buscar a propriedade do model que representa a quantidade
+ * @param {Object} model Objeto que representa o item a ser marcado
+ * @param {String} modelTarget Identifica a propriedade que identifica unicamente o item corrente no completeModel
+ * @param {Boolean} parentChecked Representa o flag usado para marcar/desmarcar o checkbox de todos os itens
  *
  * Exemplo:
  *
  *     @example
- *     <rc-checkbox complete-model="completeModel" model="model" target="target[$index]" response-model="responseModel" check-all="false"></rc-checkbox>
+ <rc-checkbox quantity-model="quantityModel" quantity-field="quantityField" model="model" complete-model="completeModel" check-model="checksModel" model-target="modelTargetField" response-model="responseModel" parent-checked="parentChecked"></rc-checkbox>
  *
- *     @example
- *     <rc-checkbox complete-model="completeModel" model="model" response-model="responseModel" check-all="true"></rc-checkbox>
+ * @class Conciliador.checkboxParent
+ * Diretiva de checkbox para marcar todos os itens.
  *
- *     @example
- *     <rc-checkbox response-model="responseModel" action-button="true" action-button-prefix="'conciliar'" action-button-sufix="'vendas'" action-button-on-click="teste"></rc-checkbox>
+ * Utilize a diretiva rc-checkbox-parent para marcar/desmarcar todos os itens de uma só vez
+ *
+ * @param {Object} quantityModel armazena a quantidade de itens marcados
+ * @param {Object} completeModel Espera o model de objeto com todas as opções disponíveis
+ * @param {Object} checkModel Model que será utilizado para compartilhar as opções checkadas com outros checkboxes
+ * @param {String} modelTarget Identifica a propriedade que identifica unicamente o item corrente no completeModel
+ * @param {Array} responseModel que será utilizado para devolver a resposta em array
+ * @param {Boolean} checked Representa o flag usado para marcar/desmarcar o checkbox de todos os itens
+ * @param {String} quantityField usado para buscar a propriedade do model que representa a quantidade
+ *
+ * Exemplo
+ *    @example
+ *    <rc-checkbox-parent quantity-model="quantityModel" complete-model="completeModel" check-model="checkModel" model-target="modelTarget" response-model="responseModel" checked="checked" quantity-field="quantityField"></rc-checkbox-parent>
+ *
+ * @class Conciliador.countButton
+ * Diretiva de botão com contagem de itens marcados
+ *
+ * Utilize a diretiva rc-count-button para renderizar um botão com label dinâmico que conta itens automaticamente e
+ * é habilitado/desabilitado automaticamente de acordo com a quantidade de itens marcados
+ *
+ * @param {String} labelPrefix Usado para montar o label do botão
+ * @param {String} labelSuffix Usado para montar o label do botão
+ * @param {Integer} itemCount Usado para montar o label do botão e para habilitar ou desabilitar o botão
+ * @param {Event} onClick Evento lançado ao clicar no botão
+ *
+ * @example
+ * <rc-count-button label-prefix="labelPrefix" label-suffix="labelSuffix" item-count="itemCount" on-click="onClick()"></rc-count-button>
  */
 
 (function() {
@@ -33,23 +60,24 @@
     angular
         .module('Conciliador')
         .directive('rcCheckbox', RcCheckbox)
+        .directive('rcCheckboxParent', RcCheckboxParent)
+        .directive('rcCountButton', RcCountButton);
 
     function RcCheckbox() {
 
         return {
             restrict: 'E',
-            templateUrl : 'app/views/directives/rc-checkbox.html',
+            templateUrl: 'app/views/directives/rc-checkbox.html',
             transclude: true,
             scope: {
                 completeModel: "=",
+                quantityModel: "=",
+                quantityField: "@",
                 model: "=",
-                target: "=",
+                checkModel: "=",
+                modelTarget: "@",
                 responseModel: "=",
-                checkAll: "=",
-                actionButton: "=",
-                actionButtonPrefix: "=",
-                actionButtonSufix: "=",
-                actionButtonOnClick: "&"
+                parentChecked: "="
             },
             controller: Controller
         };
@@ -60,10 +88,13 @@
 
             function Init() {
 
-                $scope.selectAll = SelectAll;
                 $scope.selectSingle = SelectSingle;
-                $scope.allChecked = false;
+                $scope.getModelValue = GetModelValue;
+            }
 
+            function GetModelValue() {
+                var xGetModelValue = DoGetModelValue($scope.model, $scope.modelTarget);
+                return xGetModelValue;
             }
 
             /**
@@ -72,55 +103,113 @@
              */
             function SelectSingle() {
 
-                DoSelectSingle($scope.target);
-                $scope.allChecked = $scope.model.length === $scope.completeModel.length;
-
+                var xModelValue = DoGetModelValue($scope.model, $scope.modelTarget);
+                DoSelectSingle($scope.model, xModelValue, $scope);
+                $scope.parentChecked = $scope.responseModel.length === $scope.completeModel.length;
             }
+        }
 
-            /**
-             * @method DoSelectSingle
-             * @param {Object} objSelected, objeto com um parametro id
-             * função responsável pela lógica de marcar o checkbox && colocar na model
-             */
-            function DoSelectSingle(objSelected) {
+    }
 
-                var intId = objSelected.id;
-                var intQuantity = $scope.completeModel.length;
-                if ($scope.model[intId] === true) {
-                    var intItemIndex = $scope.responseModel.indexOf(intId);
-                    $scope.model[intId] = false;
-                    $scope.responseModel.splice(intItemIndex, 1);
-                } else {
-                    $scope.model[intId] = true;
-                    $scope.responseModel.push(intId);
-                }
+    function RcCheckboxParent() {
+         return {
+             restrict: 'E',
+             templateUrl: 'app/views/directives/rc-checkbox-parent.html',
+             transclude: true,
+             scope: {
+                 completeModel: "=",
+                 checkModel: "=",
+                 modelTarget: "@",
+                 checked: "=",
+                 responseModel: "=",
+                 quantityField: "@",
+                 quantityModel: "="
+             },
+             controller: Controller
+         };
 
-                console.log("$scope.responseModel.lenght", $scope.responseModel.lengthlength)
-                console.log("$scope.responseModel", $scope.responseModel)
+         function Controller($scope) {
+             Init();
 
-            }
+             function Init() {
+                 $scope.selectAll = SelectAll;
+                 $scope.modelTarget = $scope.modelTarget ? $scope.modelTarget : 'id';
+             }
 
+             /**
+              * @method SelectAll
+              * função responsável por selecionar/deselecionar todos os checkboxes da model
+              */
+             function SelectAll() {
 
-            /**
-             * @method SelectAll
-             * função responsável por selecionar/deselecionar todos os checkboxes da model
-             */
-            function SelectAll() {
+                 var bolOldValue = $scope.checked;
+                 $scope.checked = !bolOldValue;
 
-                var bolOldValue = $scope.allChecked;
-                $scope.allChecked = !bolOldValue;
+                 $scope.completeModel.forEach(function (objItem) {
+                     var xModelValue = DoGetModelValue(objItem, $scope.modelTarget);
+                     var bolCurrent = $scope.checkModel[xModelValue];
+                     if (bolCurrent !== $scope.checked) {
+                         DoSelectSingle(objItem, xModelValue, $scope);
+                     }
+                 });
+             }
+         }
+    }
 
-                $scope.completeModel.forEach(function (objItem) {
-                    var bolCurrent = $scope.model[objItem.id];
-                    if (bolCurrent !== $scope.allChecked) {
-                        DoSelectSingle(objItem);
-                    }
-                });
+    function RcCountButton() {
+        return {
+            restrict: 'E',
+            templateUrl: 'app/views/directives/rc-count-button.html',
+            transclude: true,
+            scope: {
+                label: '=',
+                labelPrefix: "=",
+                labelSuffix: "=",
+                itemCount: '=',
+                onClick: "&"
+            },
+            controller: Controller
+        };
+
+        function Controller($scope) {
+
+            Init();
+
+            function Init() {
 
             }
 
         }
+    }
 
+    /**
+     * @method DoSelectSingle
+     * @param {Object} objSelected, objeto com um parametro id
+     * função responsável pela lógica de marcar o checkbox && colocar na model
+     */
+    function DoSelectSingle(objSelected, xSelectedVal, objScope) {
+
+        var intQuantity = objScope.completeModel.length;
+        if (objScope.quantityField !== undefined) {
+            intQuantity = objSelected[objScope.quantityField];
+        }
+
+        if (objScope.checkModel[xSelectedVal] === true) {
+            var intItemIndex = objScope.responseModel.indexOf(xSelectedVal);
+            objScope.checkModel[xSelectedVal] = false;
+            objScope.responseModel.splice(intItemIndex, 1);
+            objScope.quantityModel -= intQuantity;
+        } else {
+            objScope.checkModel[xSelectedVal] = true;
+            objScope.responseModel.push(xSelectedVal);
+            objScope.quantityModel += intQuantity;
+        }
+
+    }
+
+    function DoGetModelValue(objItem, objTargetField) {
+        var xModelValue = eval('objItem.' + objTargetField);
+        return xModelValue;
     }
 
 })();
