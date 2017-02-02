cd74993dc3fbebd9fb304b61fa3d4c4cdec22e2e
ITERIS\marco.anjos <marco.anjos@iteris.com.br>
2017-02-02 18:55:47 -0200

    voltando o dashboard-controller a como era antes

--- WebContent/app/controllers/dashboard-controller.js
+++ WebContent/app/controllers/dashboard-controller.js
@@ -20,11 +20,11 @@ angular.module('Conciliador.dashboardController',[])
 	$scope.currentPeriodStartDateYear = calendarFactory.getYearOfDate(calendarFactory.getActualDayOfCurrentMonthForDashboard());
 
 	$scope.prevPeriodStartDateDay = calendarFactory.getDayOfMonth(calendarFactory.getFirstDayOfLastMonthForDashboard());
-	$scope.prevPeriodEndDateDay = calendarFactory.getDayOfMonth(calendarFactory.getLastDayOfLastMonthForDashboard());
+	$scope.prevPeriodEndDateDay = calendarFactory.getDayOfMonth(calendarFactory.getActualDayOfLastMonthForDashboard());
 	$scope.prevPeriodStartDateMonth = calendarFactory.getMonthNameOfDate(calendarFactory.getFirstDayOfLastMonthForDashboard());
 	$scope.prevPeriodStartDateYear = calendarFactory.getYearOfDate(calendarFactory.getFirstDayOfLastMonthForDashboard());
 
-	$scope.prevPeriodEndDateDayMovement = calendarFactory.getDayOfMonth(calendarFactory.getLastDayOfLastMonthForDashboard());
+	$scope.prevPeriodEndDateDayMovement = calendarFactory.getDayOfMonth(calendarFactory.getActualDayOfLastMonthForDashboard());
 
 	$scope.sales = Sales;
 
