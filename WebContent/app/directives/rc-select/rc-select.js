9b7477ef785c16bd244dfb23bb80ff0d53949c49
Jony Fernando Schulz <jony.schulz@iteris.com.br>
2017-03-27 13:53:24 -0300

    Make tests for pvList when add show-list

--- WebContent/app/directives/rc-select/rc-select.js
+++ WebContent/app/directives/rc-select/rc-select.js
@@ -67,7 +67,6 @@
 			vm.groupSelected = GroupSelected;
 			vm.pvGroups = [];
 			vm.elementTrigger = $element[0].children[0];
-			console.log($element)
 
 			Init();
 			OpenPlaceholder();
@@ -452,7 +451,7 @@
 
 			/**
 			 * @method OpenPlaceholder
-			 * Após o click no input ele retorna a listagem de PVs.
+			 * Após o click no input ele retorna a listagem de PVs adicionando a classe show-list.
 			 *
 			 */
 			function OpenPlaceholder() {
